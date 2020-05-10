06a031e92377617c27910e84eff9686076d8637d
Dmitry Fedorov <dm1tr.f3d0rov@yandex.ru>
2020-05-10 12:25:04 +0300

    feature: сделать ветку js

--- script.js
+++ script.js
@@ -2,19 +2,18 @@ let element = document.querySelector('.profile__edit-button');
 function openClick() {
     let rank = document.querySelector('.popup');
     rank.className = 'popup popup_opened';
-    console.log (`стили: ${rank.classList}`);
-    
- }
+
+}
 element.addEventListener('click', openClick);
 
 
 let button = document.querySelector('.popup__toggle-image');
 function closeClick() {
-    
+
     let close = document.querySelector('.popup_opened');
     close.classList.remove('popup_opened');
-    console.log (`стили: ${close.classList}`);
-  
- }
+
+
+}
 
 button.addEventListener('click', closeClick);
