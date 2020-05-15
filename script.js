77456a081004f794dd528d58cf2a5a994e6179f6
Dmitry Fedorov <dm1tr.f3d0rov@yandex.ru>
2020-05-15 13:07:16 +0300

    не работает попап

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 
-const popup = document.querySelector('.popup');
 const element = document.querySelector('.profile__edit-button');
-const button = document.querySelector('.popup__toggle-image');
+const popup = document.querySelector('.popup');
+const button = document.querySelector('.popup__toggle');
 const formElement = document.querySelector('.form');
-const nameInput = document.querySelector('.popup__item_name');
-const jobInput = document.querySelector('.popup__item_occupation');
-const name = document.querySelector('.profile__name');
-const job = document.querySelector('.profile__occupation');
+let nameInput = document.querySelector('.popup__item_name');
+let jobInput = document.querySelector('.popup__item_occupation');
+let name = document.querySelector('.profile__name');
+let job = document.querySelector('.profile__occupation');
 
 function closeClick() {
-    popup.classList.toggle('popup_opened')
-    nameInput.value = name.textContent;
-    jobInput.value = job.textContent;
+   if (popup.classList.contains('popup_opened')) {
+    popup.classList.toggle('popup_opened');
+    
 }
+    
+    else {
+    popup.classList.toggle('popup_opened');
+    nameInput.value = name.textContent;
+    jobInput.value = job.textContent;}
+  
+    }
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
